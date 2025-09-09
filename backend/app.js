993508818c62cfce65b50c8414935dd2bcca3704
Eduardo <edu.denegri@gmail.com>
2025-09-09 16:54:48 -0300

    changes due to cors

--- backend/app.js
+++ backend/app.js
@@ -4,21 +4,13 @@ const routerFile = require("./routes")
 const cors = require("cors")
 const PORT = process.env.PORT || 3000;
 
-const allowedOrigins = [
-  "https://blog-front-write.onrender.com",
-  "https://blog-app-1-2fcm.onrender.com"
-];
 
 app.use(cors({
-  origin: function(origin, callback){
-    // Permitir requests sin origin (por ejemplo desde Postman o backend)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.includes(origin)){
-      return callback(null, true);
-    } else {
-      return callback(new Error("Not allowed by CORS"));
-    }
-  }
+  origin: [
+  "https://blog-front-write.onrender.com",
+  "https://blog-app-1-2fcm.onrender.com"
+  ],
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
 }));
 
 
