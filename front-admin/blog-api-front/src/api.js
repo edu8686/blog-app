3f3e1d36bdf409d58ef1d31d37f4d41b657b32d5
Eduardo <edu.denegri@gmail.com>
2025-09-09 16:27:01 -0300

    Rename front folders to remove parentheses

--- front-admin/blog-api-front/src/api.js
+++ front-admin/blog-api-front/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = "https://blog-app-69pn.onrender.com";
 
 export async function login(username, password) {
   const res = await fetch(`${API_URL}/login`, {
